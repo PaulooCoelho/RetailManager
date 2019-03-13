f3a6629f787943a2f1797532dac4a3708e49884c
Paulo Coelho <paulo.coelho@adentis.pt>
2019-03-13 10:15:54 +0000

    Stop tracking bin folder files Added login functionalities

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
-       
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+        public bool CanLogIn(string username, string password)
+        {
+            bool output = false;
+
+            if (username.Length > 0 && password.Length > 0)
+            {
+                output = true;
+            }
+
+            return output;
+        }
+
+        public void LogIn(string username, string password)
+        {
+            Console.WriteLine();
+        }
     }
 }
